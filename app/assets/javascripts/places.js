142e21f731dac337c19993235b8ce0cbe1d12ffc
Paulo Belo <pjbelo@gmail.com>
2018-09-22 18:46:39 +0100

    insert a marker

--- app/assets/javascripts/places.js
+++ app/assets/javascripts/places.js
@@ -4,8 +4,15 @@
 function initMap(lat, lng) {
     var myCoords = new google.maps.LatLng(lat, lng);
     var mapOptions = {
-    center: myCoords,
-    zoom: 14
+        center: myCoords,
+        zoom: 14
     };
+
     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    var marker = new google.maps.Marker({
+        position: myCoords,
+        map: map
+    });
+    
 }

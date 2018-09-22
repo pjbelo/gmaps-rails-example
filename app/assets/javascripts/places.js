1ec6e241f0283ffc2605b1ec94d1ea5487ae819f
Paulo Belo <pjbelo@gmail.com>
2018-09-22 18:42:55 +0100

    Using Google Maps Javascript API

--- app/assets/javascripts/places.js
+++ app/assets/javascripts/places.js
@@ -1,2 +1,11 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+function initMap(lat, lng) {
+    var myCoords = new google.maps.LatLng(lat, lng);
+    var mapOptions = {
+    center: myCoords,
+    zoom: 14
+    };
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+}
